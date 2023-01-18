edd854529a5939701e2230e88fcf42eaa6fdd51d
GeTtOo <gettoo953@gmail.com>
2023-01-18 13:27:24 +0300

    Added modules and settings support

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
+import FS from "fs";
+import Path from "path";
 import Express, { Application, Request, Response } from "express";
-import { AppDataSource } from "./data-source"
-import { Players } from "./entity/Players"
-import { Inventory } from "./entity/Inventory";
+import { AppDataSource } from "./utils/data-source"
+import { IGlobalConfig, Settings } from "./utils/settings"
 
+const Config = new Settings('core', { port: 3000, useStatic: false, staticDir: "" } as IGlobalConfig).getGlobalConfig();
 const app: Application = Express();
-const port = 3000;
+const port: number = Config.port;
 
 AppDataSource.initialize().catch((err) => {
 	console.error(`Error during Data Source initialization: ${err}`);
+	process.exit(1);
 });
 
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
+if(Config.useStatic) app.use(Express.static(Config.staticDir));
 
 app.get('/', async (req: Request, res: Response): Promise<Response> => {
-	const user = await AppDataSource.manager.findOneBy(Players, {id: 3});
+	/*const user = await AppDataSource.manager.findOneBy(Players, {id: 3});
 	
 	if(user === null)
 		return res.status(404).end();
@@ -22,26 +26,42 @@ app.get('/', async (req: Request, res: Response): Promise<Response> => {
 	const item: Inventory = new Inventory();
 	item.player = user;
 	item.item = { name: "Test", description: "test item", timeStart: 0, timeEnd: 0, meta: {}};
-	await AppDataSource.manager.save(item);
+	await AppDataSource.manager.save(item);*/
 	
-	return res.status(200).send(item);
+	return res.status(200).send({ status: 200});
 });
 
-try {
-	app.listen(port, (): void => {
-		console.log(`Server successfully start on port ${port}`);
-
-		/*
-		const eventsPath = path.join(process.cwd(), 'modules');
-		const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-		for (const file of eventFiles) {
-			const filePath = path.join(eventsPath, file);
-			const event = require(filePath);
-			console.log(event);
-			event.execute();
-		}*/
-	});
-} catch(err) {
-	console.error(`Error when starting server: ${err}`);
+async function StartApp() {
+	try {
+		app.listen(port, (): void => {
+			console.log(`Server successfully start on port ${port}`);
+			LoadModules();
+		});
+	} catch(err) {
+		console.error(`Error when starting server: ${err}`);
+	}
 }
+
+async function LoadModules() {
+	const modulesPath: string = Path.join(process.cwd(), 'modules');
+
+	if(FS.existsSync(modulesPath)) {
+		const modulesFiles: string[] = FS.readdirSync(modulesPath).filter(file => file.endsWith('.js'));
+		
+		for (const file of modulesFiles) {
+			try {
+				const filePath: string = Path.join(modulesPath, file);
+				const module = require(filePath);
+				
+				if(module.type === 'router')
+					app.use('/' + module.name, module.router);
+			} catch(err) {
+				console.error(`Error loading module ${file}: ` + err);
+			}
+		}
+	} else {
+		FS.mkdirSync(modulesPath, { recursive: true });
+	}
+}
+
+StartApp();
