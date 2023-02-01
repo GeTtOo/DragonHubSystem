d7e7baf5d5cbee2df22d0e53a692a65104275aed
GeTtOo <gettoo953@gmail.com>
2023-02-01 19:36:32 +0300

    Added news

---
+++ app/frontend/src/components/NewsSlide.vue
@@ -0,0 +1,65 @@
+<template>
+<div class="row">
+	<div class="col-lg">
+		<div class="card">
+			<div class="card-header text-center">
+				<strong>News</strong>
+			</div>
+			<div class="list-group list-group-flush">
+				<div class="list-group-item">
+					<div class="card mb-3">
+						<div class="row g-0">
+							<div class="col-md-4 news_image rounded-start"></div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title">News 1</h5>
+									<p class="card-text">The quick brown fox jumps over the lazy dog. Jackdaws love my big sphinx of quartz. Pack my box with five dozen liquor jugs.</p>
+									<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card mb-3">
+						<div class="row g-0">
+							<div class="col-md-4 news_image rounded-start"></div>
+							<div class="col-md-8">
+							<div class="card-body">
+								<h5 class="card-title">News 2</h5>
+								<p class="card-text">The quick brown fox jumps over the lazy dog. Jackdaws love my big sphinx of quartz. Pack my box with five dozen liquor jugs.</p>
+								<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+							</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: "NewsSlide"
+})
+</script>
+
+<style lang="scss" scoped>
+	.card-header {
+		color: $white;
+		background-color: $gray-800;
+		border-bottom: 2px solid $orange-500;
+	}
+
+	.list-group-item {
+		background-color: $gray-100;
+	}
+
+	.news_image {
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-image: url("@/assets/dust2.jpg");
+	}
+</style>
--- app/frontend/src/views/HomeView.vue
+++ app/frontend/src/views/HomeView.vue
@@ -14,66 +14,74 @@
 		</div>
 	</div>
 </div>
+<div class="container mt-3">
+	<div class="row">
+		<div class="col-lg mb-2 mx-auto">
+			<NewsSlide/>
+		</div>
+	</div>
+</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import ServerStatus from '@/components/ServerStatus.vue';
 import ListGroup from '@/components/ListGroup.vue';
-import IListItems from '@/types/IListItems'
+import IListItems from '@/types/IListItems';
+import NewsSlide from '@/components/NewsSlide.vue';
 
 const lastPlayers: IListItems = {
 	name: "Last Players", 
 	list: [
-		{id: 0, name: "Player 0"},
-		{id: 1, name: "Player 1"},
-		{id: 2, name: "Player 2"},
-		{id: 3, name: "Player 3"},
-		{id: 4, name: "Player 4"},
-		{id: 5, name: "Player 5"},
-		{id: 6, name: "Player 6"},
-		{id: 7, name: "Player 7"},
-		{id: 8, name: "Player 8"},
-		{id: 9, name: "Player 9"}
-	]
+		{id: 0, name: "Player 0", url: "1"},
+		{id: 1, name: "Player 1", url: "1"},
+		{id: 2, name: "Player 2", url: "1"},
+		{id: 3, name: "Player 3", url: "1"},
+		{id: 4, name: "Player 4", url: "1"},
+		{id: 5, name: "Player 5", url: "1"},
+		{id: 6, name: "Player 6", url: "1"},
+		{id: 7, name: "Player 7", url: "1"},
+		{id: 8, name: "Player 8", url: "1"},
+		{id: 9, name: "Player 9", url: "1"}
+	],
 }
 
 const adminAction: IListItems = {
 	name: "Admin Actions", 
 	list: [
-		{id: 0, name: "Admin Player 10 Kick Player 0"},
-		{id: 1, name: "Admin Player 10 Kick Player 1"},
-		{id: 2, name: "Admin Player 10 Kick Player 2"},
-		{id: 3, name: "Admin Player 10 Kick Player 3"},
-		{id: 4, name: "Admin Player 10 Kick Player 4"},
-		{id: 5, name: "Admin Player 10 Kick Player 5"},
-		{id: 6, name: "Admin Player 10 Kick Player 6"},
-		{id: 7, name: "Admin Player 10 Kick Player 7"},
-		{id: 8, name: "Admin Player 10 Kick Player 8"},
-		{id: 9, name: "Admin Player 10 Kick Player 9"}
+		{id: 0, name: "Admin Player 10 Kick Player 0", url: "1"},
+		{id: 1, name: "Admin Player 10 Kick Player 1", url: "1"},
+		{id: 2, name: "Admin Player 10 Kick Player 2", url: "1"},
+		{id: 3, name: "Admin Player 10 Kick Player 3", url: "1"},
+		{id: 4, name: "Admin Player 10 Kick Player 4", url: "1"},
+		{id: 5, name: "Admin Player 10 Kick Player 5", url: "1"},
+		{id: 6, name: "Admin Player 10 Kick Player 6", url: "1"},
+		{id: 7, name: "Admin Player 10 Kick Player 7", url: "1"},
+		{id: 8, name: "Admin Player 10 Kick Player 8", url: "1"},
+		{id: 9, name: "Admin Player 10 Kick Player 9", url: "1"}
 	]
 }
 
 const topPlayers: IListItems = {
 	name: "Top Players", 
 	list: [
-		{id: 0, name: "Player 0"},
-		{id: 1, name: "Player 1"},
-		{id: 2, name: "Player 2"},
-		{id: 3, name: "Player 3"},
-		{id: 4, name: "Player 4"},
-		{id: 5, name: "Player 5"},
-		{id: 6, name: "Player 6"},
-		{id: 7, name: "Player 7"},
-		{id: 8, name: "Player 8"},
-		{id: 9, name: "Player 9"}
+		{id: 0, name: "Player 0", url: "1"},
+		{id: 1, name: "Player 1", url: "1"},
+		{id: 2, name: "Player 2", url: "1"},
+		{id: 3, name: "Player 3", url: "1"},
+		{id: 4, name: "Player 4", url: "1"},
+		{id: 5, name: "Player 5", url: "1"},
+		{id: 6, name: "Player 6", url: "1"},
+		{id: 7, name: "Player 7", url: "1"},
+		{id: 8, name: "Player 8", url: "1"},
+		{id: 9, name: "Player 9", url: "1"}
 	]
 }
 
 export default defineComponent({
 	name: 'HomeView',
 	components: {
-		ServerStatus, ListGroup
+		ServerStatus, ListGroup, NewsSlide
 	},
 	data() {
 		return {
