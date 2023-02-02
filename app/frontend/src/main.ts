ee7dfea34b65173cb62bddd9d9e6b61ad6cb71a0
GeTtOo <gettoo953@gmail.com>
2023-02-02 03:27:39 +0300

    Font Awesome

--- app/frontend/src/main.ts
+++ app/frontend/src/main.ts
@@ -1,8 +1,16 @@
 import { createApp } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+library.add(fas)
+
+createApp(App).use(store)
+.component('fa', FontAwesomeIcon)
+.use(router)
+.mount('#app')
 
 import "bootstrap"
